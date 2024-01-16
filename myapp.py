8a720d3ef4e4bcdbee53fe7f37f497049076428b
ravisivamf <ravnisha@gmail.com>
2024-01-16 12:53:40 -0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -6,7 +6,7 @@ def main():
     logging.basicConfig(filename='myapp.log', level=logging.INFO)
     logging.info('Started')
     mylib.do_something()
-    logging.info('Finished')
+    logging.info('Finished2')
 
 if __name__ == '__main__':
     main()
