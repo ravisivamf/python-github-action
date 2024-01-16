96b23641c95bd4cf7da021f294b551815f82e185
ravisivamf <ravnisha@gmail.com>
2024-01-16 13:04:44 -0600

    Update simple.py

--- simple.py
+++ simple.py
@@ -33,8 +33,3 @@ if __name__ == "__main__":
         temperature = data["forecast"]["temp"]
         logger.info(f'Weather in Irving, TX: {temperature}')
 
-r = requests.get('https://weather.talkpython.fm/api/weather?city=irving&state=TX&country=US&units=imperial')
-if r.status_code == 200:
-   data = r.json()
-   temperature = data["forecast"]["temp"]
-   print(f'Weather in Irving, TX: {temperature}')
